51831af9be79b462f8d28f3644f017db5f0d0a4c
MOHANKUMARM11 <mohanm9612@gmail.com>
2026-06-14 19:05:45 +0530

    authentication validation completed

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -23,7 +23,7 @@ const userSchema = new mongoose.Schema(
 
     role: {
       type: String,
-      enum: ["student", "company", "admin"],
+      enum: ["student", "company"],
       default: "student",
     },
   },
--- server/src/modules/auth/auth.routes.js
+++ server/src/modules/auth/auth.routes.js
@@ -2,9 +2,27 @@ const express = require("express");
 
 const { signup, login } = require("./auth.controller");
 
+const {
+  signupValidation,
+  loginValidation,
+} = require("../../validators/auth.validator");
+
+const validate = require("../../middlewares/validation.middleware");
+
 const router = express.Router();
 
-router.post("/signup", signup);
-router.post("/login", login);
+router.post(
+  "/signup",
+  signupValidation,
+  validate,
+  signup
+);
+
+router.post(
+  "/login",
+  loginValidation,
+  validate,
+  login
+);
 
 module.exports = router;
